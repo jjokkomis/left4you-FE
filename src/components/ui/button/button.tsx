46aa4e83faf02d57152fdbd47798bd9192d21496
dpqlsn <a01037111209@gmail.com>
2025-07-27 14:42:40 +0900

    feat(#7) :: 버튼 컴포넌트 제작

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function Btn({ children }: Props) {
+    return (
+        <Container>
+        <Button>{children}</Button>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`;
+
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    width: 50%;
+    padding: 1rem;
+    border: none;
+    background-color: #364155;
+    color: #fff;
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    font-family: "MiraeroNormal";
+`;
