01ef59a95f86334ebcf728700db844fa78bff0e7
dpqlsn <a01037111209@gmail.com>
2025-08-14 21:50:13 +0900

    fix(#27) :: 코멘트 수정

--- src/containers/ui/setting/review/review.tsx
+++ src/containers/ui/setting/review/review.tsx
@@ -17,24 +17,35 @@ export default function ReviewPage() {
 
     return (
         <S.Container>
-            <Image src="assets/back-button.svg" alt="review📝" width={20} height={20} onClick={() => router.back()}/>
+            <S.Img onClick={() => router.back()}>
+                <Image
+                    src="/assets/back-button.svg"
+                    alt="back"
+                    width={20}
+                    height={20}
+                    style={{ pointerEvents: 'none' }}
+                />
+            </S.Img>
             <S.Wrapper>
-                <S.Title> 이 코스는 어땠나요? <br /> 리뷰와 별점을 남겨주세요 </S.Title>
-                <S.SubTitle> 별점은 큰 힘이 됩니다 </S.SubTitle>
+                <S.Title>이 코스는 어땠나요?<br />리뷰와 별점을 남겨주세요</S.Title>
+                <S.SubTitle>별점은 큰 힘이 됩니다</S.SubTitle>
             </S.Wrapper>
-            <S.Star>
-                {[1,2,3,4,5].map(n =>
+            <S.StarWrapper>
+                {[1,2,3,4,5].map(n => (
                     <S.Star key={n} onClick={() => setR(n)}>
-                    {n <= count ? "★" : "☆"}
+                        {n <= count ? "★" : "☆"}
                     </S.Star>
-                )}
-            </S.Star>
+                ))}
+            </S.StarWrapper>
             <S.Wrapper>
                 <S.Title>리뷰 작성</S.Title>
-                <S.ReviewTitle placeholder='리뷰 제목을 적어주세요'/>
-                <S.Review placeholder='리뷰를 작성해주세요'/>
+                <S.ReviewTitle placeholder='리뷰 제목을 적어주세요' />
+                <S.Review 
+                    placeholder='리뷰를 작성해주세요' 
+                    maxLength={200} 
+                />
             </S.Wrapper>
-            <Btn onClick={handleAlertt}> 리뷰작성 </Btn>
+            <Btn onClick={handleAlertt}>리뷰작성</Btn>
         </S.Container>
     );
 }
--- src/containers/ui/setting/review/style.ts
+++ src/containers/ui/setting/review/style.ts
@@ -15,6 +15,15 @@ export const Wrapper = styled.div`
     row-gap: 1.4rem;
 `;
 
+export const Img = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 1.5rem;
+    height: 1.5rem;
+    cursor: pointer;
+`;
+
 export const Title = styled.div`
     font-size: 1.5rem;
     line-height: 2rem;
@@ -26,25 +35,31 @@ export const SubTitle = styled.div`
     color: #B2B2B2;
 `;
 
-export const Star = styled.div`
+export const StarWrapper = styled.div`
     display: flex;
     font-size: 2.25rem;
     color: #364155;
     letter-spacing: 0.4rem;
 `;
 
+export const Star = styled.span`
+    cursor: pointer;
+`;
+
 export const ReviewTitle = styled.textarea`
     display: flex;
-    height: 5vh;
     font-size: 1rem;
-    padding: 1rem;
+    padding: 1rem 1rem 0rem 1rem;
     background-color: #FAFAFA;
     border-radius: 0.25rem;
-    border: none;
+    border: 0.1rem solid #D5D5D5;
     resize: none;
     outline: none;
+    overflow: hidden;
 
     &::placeholder {
+        display: flex;
+        justify-content: center;
         color: #B2B2B2;
     }
 `;
@@ -56,9 +71,10 @@ export const Review = styled.textarea`
     padding: 1rem;
     background-color: #FAFAFA;
     border-radius: 0.25rem;
-    border: none;
+    border: 0.1rem solid #D5D5D5;
     resize: none;
     outline: none;
+    overflow: hidden;
 
     &::placeholder {
         color: #B2B2B2;
