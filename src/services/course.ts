5159863924a6915f5155e37627afe84ebb4eee37
dpqlsn <a01037111209@gmail.com>
2025-08-17 15:30:12 +0900

    feat(#34) :: 백엔드 연결

--- src/services/course.ts
+++ src/services/course.ts
@@ -1,5 +1,5 @@
 import customAxios from "@/lib/customAxios";
-import type { CreateCourse, Response } from "@/types/types";
+import type { CreateCourse, Response, UpdateCourse } from "@/types/types";
 
 const createCourse = async (
     data: CreateCourse
@@ -13,5 +13,22 @@ async function getCourseList() {
     return response.data;
 }
 
-export { createCourse, getCourseList };
+async function getCourseReviewList() {
+    const response = await customAxios.get("/course/review");
+    return response.data;
+}
+
+const updateCourse = async (
+    data: UpdateCourse
+    ): Promise<Response> => {
+    const response = await customAxios.put<Response>("/course/update", data);
+    return response.data;
+};
+
+const getCourseById = async (course_id: number) => {
+    const response = await customAxios.get<Response>(`/course/${course_id}`);
+    return response.data;
+};
+
+export { createCourse, getCourseList, getCourseReviewList, updateCourse, getCourseById };
 export default createCourse;
