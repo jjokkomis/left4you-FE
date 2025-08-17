6e5994b4f6821040b9a0cf052af89591d971250e
dpqlsn <a01037111209@gmail.com>
2025-08-17 16:46:54 +0900

    feat(#34) :: 이때동안 선물 받은 코스

--- src/containers/ui/setting/mypage/mypage.tsx
+++ src/containers/ui/setting/mypage/mypage.tsx
@@ -4,46 +4,51 @@ import * as S from './style';
 import { useRouter } from 'next/navigation';
 import useCourse from "@/hooks/useCourse";
 import { useEffect, useState } from "react";
-import { getCourseReviewList } from "@/services/course";
-import type { CourseReview } from "@/types/types";
+import { getCourseGift } from "@/services/course";
+import type { CourseGift, CourseReview } from "@/types/types";
 
 export default function Setting() {
     const router = useRouter();
     const { courseList: data } = useCourse();
-
-    const [, setReceivedCourses] = useState<CourseReview[]>([]);
+    const [receivedCourses, setReceivedCourses] = useState<CourseGift[]>([]);
 
     useEffect(() => {
         async function fetchReceivedCourses() {
-            try {
-                const res = await getCourseReviewList();
-                setReceivedCourses(res.courses || []);
-            } catch (error) {
-                console.error("선물 받은 코스 불러오기 실패:", error);
-            }
+        const courseId = data?.courses?.[0]?.id;
+        if (!courseId) return;
+        const res = await getCourseGift(courseId);
+        setReceivedCourses(res.courses || []);
         }
         fetchReceivedCourses();
-    }, []);
+    }, [data?.courses]);
 
     return (
         <S.Container>
-            <S.Title>마이페이지</S.Title>
-            <S.Wrapper>
-                <S.SubTitle>이때동안 만든 코스</S.SubTitle>
-                {data?.courses?.map((course) => (
-                    <S.Box
-                        key={course.id}
-                        onClick={() => router.push(`/review/${course.id}`)}
-                    > 
-                        <S.CourseName>{course.name}</S.CourseName>
-                    </S.Box>
-                ))}
-            </S.Wrapper>
-            <S.Wrapper>
-                <S.SubTitle>이때동안 선물 받은 코스</S.SubTitle>
-                <S.Box onClick={() => router.push('/review')} />
-                <S.Box onClick={() => router.push('/review')} />
-            </S.Wrapper>
+        <S.Title>마이페이지</S.Title>
+
+        <S.Wrapper>
+            <S.SubTitle>이때동안 만든 코스</S.SubTitle>
+            {data?.courses?.map((course: CourseReview) => (
+            <S.Box
+                key={course.id}
+                onClick={() => router.push(`/review/${course.id}`)}
+            >
+                <S.CourseName>{course.name}</S.CourseName>
+            </S.Box>
+            ))}
+        </S.Wrapper>
+
+        <S.Wrapper>
+            <S.SubTitle>이때동안 선물 받은 코스</S.SubTitle>
+            {receivedCourses.map((course: CourseGift) => (
+            <S.Box
+                key={course.id}
+                onClick={() => router.push(`/review/${course.course_id}`)}
+            >
+                <S.CourseName>{course.course_name}</S.CourseName>
+            </S.Box>
+            ))}
+        </S.Wrapper>
         </S.Container>
     );
 }
