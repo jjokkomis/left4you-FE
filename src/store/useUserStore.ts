c96184fd4e656c2c39a38917398e793cb0a841ad
zuu3 <ojh27467242@gmail.com>
2025-08-18 01:24:08 +0900

    refactor(#36) :: 유저 저장 로직 변경

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,18 +1,23 @@
 import { create } from "zustand";
-
+import { persist } from 'zustand/middleware';
 
 import { UserState } from '@/types/auth';
 
-export const useUserStore = create<UserState>((set) => ({
-  user: null,
-
-  setUser: (user) =>
-    set(() => ({
-      user,
-    })),
-
-  clearUser: () =>
-    set(() => ({
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
       user: null,
-    })),
-}));
+
+      setUser: (user) =>
+        set(() => ({
+          user,
+        })),
+
+      clearUser: () =>
+        set(() => ({
+          user: null,
+        })),
+    }),
+    { name: 'user-store' }
+  )
+);
